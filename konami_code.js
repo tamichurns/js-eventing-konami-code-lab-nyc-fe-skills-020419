6c6544b67c537a57b8bac3c82642df4c4aa8b05e
Tami Churns <tchurns@paperlesspost.com>
2019-03-27 22:19:50 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 let tracker = 0
 
 function init() {
-  if (event.keyCode) === code[tracker]) {
-  } else {
-    
+  
+  function onKeyDown(event) {
+    console.log(event)
   }
+  
+  document.addEventListener('keydown', onKeyDown)
 }

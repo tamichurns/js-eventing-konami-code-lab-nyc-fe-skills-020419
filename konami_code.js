92861a3120c34a5ff8c9aa309eaf03a700d93c37
Tami Churns <tchurns@paperlesspost.com>
2019-03-27 22:45:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let tracker = 0
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init() {}
+  
+function onKeyDown(event) {
+    if (event.keyCode === code[tracker]){
+      tracker++
+    }  else {
+      tracker = 0 
+    }
+    if (tracker === 10) {
+      alert('nice jerb')
+    }
+  }
 
-}
+document.addEventListener('keydown', onKeyDown)
+
+init() 

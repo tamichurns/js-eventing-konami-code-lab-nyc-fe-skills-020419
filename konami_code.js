05a1541601ff454dc0b09d9797879e5044bcebda
Tami Churns <tchurns@paperlesspost.com>
2019-03-27 22:22:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -6,8 +6,8 @@ function init() {
   function onKeyDown(event) {
     console.log(event)
   }
-  
-
 }
 
-  document.addEventListener('keydown', onKeyDown)
+document.addEventListener('keydown', onKeyDown)
+
+init() 
